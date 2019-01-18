7e2941cb1aca005b4dfbf2f9b4cf7904aa49b219
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2019-01-18 22:15:47 +0530

    Number of tiles required to cover a floor

---
+++ q15.py
@@ -0,0 +1,9 @@
+            #To find Number of tiles required to cover a floor
+lenf=eval(input("Enter the length of floor "))
+bref=eval(input("Enter the breadth of floor "))
+lent=eval(input("Enter the length of tile "))
+bret=eval(input("Enter the breadth of tile "))
+areaf=lenf*bref
+areat=lent*bret
+reqt=areaf//areat
+print("No. of tiles required to cover the floor is ",reqt)
