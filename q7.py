e28c0e068f36ab868adae00407183e3700dea42d
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2019-01-19 17:52:21 +0530

    To find the area and perimeter of rectangle

---
+++ q7.py
@@ -0,0 +1,6 @@
+            #To find area and perimeter of rectangle
+length=int(input("Enter the length "))
+breadth=int(input("Enter the breadth "))
+area=length*breadth
+perimeter=(2*(length+breadth))
+print("Area=",area,"Perimeter=",perimeter)
