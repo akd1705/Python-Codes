ab4b16976a3c736920c72d4c68890f5d83298fd6
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2019-01-19 17:48:34 +0530

    To find the area and circumference of circle

---
+++ q8.py
@@ -0,0 +1,5 @@
+          #Program to find the area and circumference of circle
+radius=eval(input("Enter radius"))
+area=(3.14*(radius*radius))
+circum=(2*3.14*radius)
+print("Area=",area,"Circumference",circum)
