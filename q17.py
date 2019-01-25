5be9a2644f3c23f36197034f49138cb82d0cbaa6
Aditya Kumar Dubey <43343014+akd1705@users.noreply.github.com>
2019-01-25 22:17:54 +0530

    To find the numbers of goats and chickens

---
+++ q17.py
@@ -0,0 +1,5 @@
+head=int(input("Enter the number of Heads"))
+feet=int(input("Enter the number of Feet"))
+goat=(feet/2)-head
+chick=head-goat
+print("Number of Goats=",goat,"Number of Chickens=",chick)
